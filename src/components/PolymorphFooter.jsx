da8722b067cef3e38b93c3af5142a8848494a40d
Daniël Botha <daniel@polymorph.co.za>
2020-10-26 22:27:50 +0200

    init

---
+++ src/components/PolymorphFooter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import polymorphLogo from '../img/polymorph-logo.png';
+import { Image } from 'mdx-deck';
+
+function PolymorphFooter() {
+  return (
+    <div>
+      <Image src={polymorphLogo}
+        style={{
+          width: 300
+        }}
+      />
+      asdf
+    </div>
+  )
+}
+
+export default PolymorphFooter
---
+++ src/components/PolymorphHeader.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import polymorphLogo from '../img/polymorph-logo.png';
+import { Image } from 'mdx-deck';
+
+function PolymorphHeader() {
+  return (
+    <div>
+      <Image src={polymorphLogo}
+        style={{
+          width: 400,
+          height: 100
+        }}
+      />
+      asdf
+    </div>
+  )
+}
+
+export default PolymorphHeader
